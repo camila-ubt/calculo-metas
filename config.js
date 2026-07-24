e8c1d0ae4fe85deab1d66480392ca300eac1e163
Camila S. <cnstos@gmail.com>
2026-07-24 00:39:05 -0300

    Conecta calculadora ao Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://bqctcmqnewyfwuiqtnmb.supabase.co",
+  supabaseAnonKey: "sb_publishable_k7ZWFIieW4zPRKGRfSNHAg_ZD2Xc20N",
 };
