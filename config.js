8210c77dce4cd1128e1f64736abcecfe6a4c5bf5
Camila S. <cnstos@gmail.com>
2026-08-27 23:28:08 -0300

    Remove redirecionamento legado de metas

--- config.js
+++ config.js
@@ -3,20 +3,6 @@ window.APP_CONFIG = {
   supabaseAnonKey: "sb_publishable_T9uKhKxBEekm5SZqSl7nNA_qs7pXG3g",
 };
 
-const fetchOriginal = window.fetch.bind(window);
-
-window.fetch = function fetchComMetasCompartilhadas(recurso, opcoes) {
-  const url = typeof recurso === "string" ? recurso : recurso?.url || "";
-  const consultaAntiga = "/rest/v1/metas_mensais?select=*&order=mes.desc";
-
-  if (url.includes(consultaAntiga)) {
-    const novaUrl = `${window.APP_CONFIG.supabaseUrl}/rest/v1/rpc/metas_publicas`;
-    return fetchOriginal(novaUrl, opcoes);
-  }
-
-  return fetchOriginal(recurso, opcoes);
-};
-
 const NOMES_DOS_MESES = [
   "Janeiro",
   "Fevereiro",
